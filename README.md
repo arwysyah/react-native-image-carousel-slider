f803ce151e4e04cbdcde5a4edcb78c90e5c3ac46
Arwy Syahputra Siregar <arwysyahputrasiregar@Arwys-MacBook-Pro.local>
2020-09-08 20:26:07 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,115 @@
-# react-native-image-carousel-slider
+
+# 👨‍💻 react-native-image-carousel-slider
+
+
+<p align="center">
+  <a href="https://reactjs.org/">
+    <img
+      alt="Node.js"
+      src="https://i.udemycdn.com/course/750x422/1049092_8c52_2.jpg"
+      width="460"
+    />
+  </a>
+</p>
+
+</h1>
+
+<br/>
+
+
+
+<br/>
+
+
+
+
+
+
+
+
+
+<p align="center">
+ 
+  </a>
+  <a href="#">
+    <img title="Open Source Love" src="https://badges.frapsoft.com/os/v1/open-source.svg?v=102">
+  </a>
+  <a href="https://github.com/iyansr?tab=followers">
+    <img title="Followers" src="https://img.shields.io/github/followers/arwysyah?style=social">
+  </a>
+  <a href="https://github.com/prettier/prettier"><img src="https://img.shields.io/badge/styled_with-prettier-ff69b4.svg"></a>
+
+ 
+</p>
+
+
+## BUILD SETUP
+```bash
+
+## Installation
+
+npm install react-native-image-carousel-slider --save
+
+
+
+```
+
+
+## USAGE
+```js
+
+import ScrollImages from react-native-image-carousel-slider
+class App extends Component {
+    state={
+        active:0
+    }
+     handleMovements = ({nativeEvent}) => {
+    const slide = Math.ceil(
+      nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
+    );
+    if (slide !== this.state.active) {
+      this.setState({
+        active: slide,
+      });
+    }
+  };
+    render(){
+        return(
+            <ScrollImages  
+            screen={'DisplayFullImages'} 
+          fixedData={carDetails[1].image}
+          screenNavigation={this.props.navigation}
+          active={this.state.active}
+          handleMovements={this.handleMovements}
+        />
+
+       
+        )
+    }
+}
+```
+
+
+## Current API(Property)
+Property | Type | Description
+------------ | ------------- | -------------
+screen | PropTypes.string | (optional)Screen Navigation and will passing property 'detailImage' as array
+fixedData | PropTypes.array | (required) Array Of Image
+screenNavigation| PropTypes.function | (optional) navigation function
+active | PropTypes.number| display active image that choosed
+handleMovements | PropTypes.object | Native Event
+
+
+##  Project Documentation
+
+
+- Author Account
+
+```bash
+Arwy Syahputra Siregar
+github.com/arwysyah
+
+
+<p > Copyright ©</p> <p style="color:green;">by Arwy Syahputra Siregar</p>
+
+
--- index.js
+++ index.js
@@ -1,8 +1,89 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import Auth from 'ho';
-const ScrollImages = createSwitchNavigator({
-    SplashScreen,
-    Home,
-    Auth,
-  });
-  export default createAppContainer(ScrollImages);
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+  Alert
+} from "react-native";
+export default class CarouselDisplay extends React.PureComponent {
+  moveDetailImage = (image, index) => {
+    let dataImage = [];
+    let newImage = dataImage.concat(image, index);
+    if(!this.props.screen){
+        Alert.alert('You Must Passing Screen Property as parameter')
+    }else{
+        this.props.screenNavigation.navigate(this.props.screen, {
+            detailImage: newImage,
+          });
+    }
+
+  };
+
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            position: "absolute",
+            alignSelf: "center",
+            marginTop: 170,
+            justifyContent: "center",
+            zIndex: 9999,
+          }}
+        >
+          {this.props.fixedData.map((i, k) => (
+            <Text
+              key={k}
+              style={
+                k === this.props.active ? styles.pagingActive : styles.paging
+              }
+            >
+              ⬤
+            </Text>
+          ))}
+        </View>
+        <ScrollView
+          pagingEnabled
+          horizontal
+          onScroll={this.props.handleMovements}
+          showsHorizontalScrollIndicator={false}
+          style={styles.backgroundImage}
+        >
+          {this.props.fixedData.map((img, index) => (
+            <TouchableOpacity
+              onPress={() =>
+                this.moveDetailImage(this.props.fixedData, img)
+              }
+            >
+              <Image
+                key={index}
+                source={{ uri: img }}
+                style={styles.backgroundImage}
+              />
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  backgroundImage: {
+    width: Dimensions.get("window").width,
+    height: (Dimensions.get("window").width * 65) / 100,
+    backgroundColor: "black",
+  },
+  paging: {
+    fontSize: 15,
+    color: "#888",
+  },
+  pagingActive: {
+    fontSize: 15,
+    color: "white",
+  },
+});
